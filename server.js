ba1139dc79aef82d33d351dff54242aee8ac9c90
Steven Miyauchi <swm9220@gmail.com>
2020-02-26 09:26:32 -0800

    cleanup

--- server.js
+++ server.js
@@ -7,68 +7,57 @@ require ('ejs');
 const superagent = require('superagent');
 
 const PORT = process.env.PORT || 3001;
-
 //tells express to use ejs
+app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true,}));
-app.use(express.static('./public'));
+
 
 app.get('/', renderHomePage);
-app.get('/newSearch', newSearch);
 app.post('/searches', collectFormData);
+app.get('/newSearch', newSearch);
 
 
 function renderHomePage(request, response) {
-    response.render('./pages/index.ejs');
+  response.render('pages/index');
+  console.log('hi');
 }
 
 function newSearch(request, response){
-    response.render('./searches/new.ejs');
-  }
+  response.render('pages/searches/new');
+}
 
 function collectFormData(request, response){
-    let formData = request.body.search;
-    let nameOfBookOrAuthor = formData[0];
-    let isTitleOrAuthor = formData[1];
-    let url = `https://www.googleapis.com/books/v1/volumes?q=`;
+  let formData = request.body.search;
+  let nameOfBookOrAuthor = formData[0];
+  let isTitleOrAuthor = formData[1];
+  let url = `https://www.googleapis.com/books/v1/volumes?q=`;
 
-    if(isTitleOrAuthor === 'title'){
-        url += `+intitle:${nameOfBookOrAuthor}`;
-    } else if (isTitleOrAuthor === 'author'){
-        url += `+inauthor:${isTitleOrAuthor}`;
-    }
+  if(isTitleOrAuthor === 'title'){
+    url += `+intitle:${nameOfBookOrAuthor}`;
+  } else if (isTitleOrAuthor === 'author'){
+    url += `+inauthor:${nameOfBookOrAuthor}`;
+  }
 
-
-    superagent.get(url)
-        .then(results => {
-            let resultsArray = results.body.items;
-            const finalArray = resultsArray.map(book => {
-                new Book(book.volumeInfo);
-            })
-            response.status(200).render('./show.ejs', {bananas: finalArray});
-        })
-        .catch(err => {
-            console.error(err);
-            response.status(500).send(err);
-        })
+  superagent.get(url)
+    .then(results => {
+      let resultsArray = results.body.items;
+      const finalArray = resultsArray.map(book => {
+        return new Book(book.volumeInfo);
+      });
+      response.status(200).render('./searches/show.ejs', {bookArryay: finalArray});
+    })
+    .catch(err => {
+      console.error(err);
+      response.status(500).send(err);
+    });
 }
 
 function Book(obj) {
-    this.title = obj.title || 'no title available';
-    this.author = obj.author || 'no author available';
-    this.image_url = obj.image_url || 'https://i.imgur.com/J5LVHEL.jpg';
-    this.description = obj.description; 
-
-app.get('/', (request, response) => {
-  // console.log('i am on!');
-  response.render('./pages/index.ejs');
-  // response.send('hello');
-});
-app.get('/newSearch', newSearch);
-
-function newSearch(request, response){
-  response.render('./searches/new.ejs');
-
+  this.title = obj.title || 'no title available';
+  this.author = obj.author || 'no author available';
+  this.image_url = obj.image_url || 'https://i.imgur.com/J5LVHEL.jpg';
+  this.description = obj.description;
 }
 
 // turn on the server
